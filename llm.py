e90cb36a465cc01b9c531fdf5647f7d4d3405bb7
Javier Chung <34190746+Javiebear@users.noreply.github.com>
2025-03-27 02:25:35 -0400

    Add files via upload

---
+++ llm.py
@@ -0,0 +1,12 @@
+# File name: llm.py
+# Date: March 26, 2025
+# Author: Javier Chung
+# Description:
+
+#brew install cmake
+# git clone https://github.com/ggerganov/llama.cpp
+# cd llama.cpp
+# make clean && make LLAMA_METAL=1
+
+if __name__ == "__main__":
+    print("Hello")
